df533ba4c8569aac407984322b66b285240e617e
Ben Hassouna <nihel.benhassouna@esprit.tn>
2018-03-15 11:00:18 +0100

    Commit with parameters.yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new ProduitBundle\ProduitBundle(),
             new MainBundle\MainBundle(),
+            new FOSUserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/FOSUserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FOSUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('FOSUserBundle:Default:index.html.twig');
+    }
+}
